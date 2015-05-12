cbe05d43bf6b93b1251feeb0b1f9404e3e8754a7
Michael Kaminsky <mkaminsky11@gmail.com>
2015-05-11 21:03:12 -0400

    ls is all done

--- js/prompted.js
+++ js/prompted.js
@@ -1,3 +1,8 @@
+//TODO: escape (allow for "<" and ">")
+//TODO: autocomplete (uh-oh)
+//TODO: cat
+//TODO: docs!
+
 /*
 * JUST FOR INIT
 */
@@ -44,7 +49,8 @@ function _prompted(elem, options){
   this.afterInput = function(e){};
   this.disable = false; //disable default output?
   this.data = [];
-  this.path = "";
+  this.path = "/";
+  this.specialExt = ["png","jpeg","JPEG","tiff","gif","mp3","mp4","mov","svg"];
 
   //prompt
   if(_prompted_helper.exists(options.prompt)){this.prompt = options.prompt}
@@ -52,7 +58,7 @@ function _prompted(elem, options){
   if(_prompted_helper.exists(options.beforeInput)){this.beforeInput = options.beforeInput}
   if(_prompted_helper.exists(options.afterInput)){this.afterInput = options.afterInput}
   if(_prompted_helper.exists(options.disable)){this.disable = options.disable}
-  if(_prompted_helper.exists(options.data)){this.readTree(options.data,"")}
+  if(_prompted_helper.exists(options.data)){this.readTree(options.data,this.path)}
 
   var main = document.createElement("DIV");
   main.className = "prompted prompted-s-default";
@@ -84,10 +90,10 @@ function _prompted(elem, options){
 
           var command = val.split(" ")[0];
           if(command === "cd"){
-            var res = this.cd(val.replace("cd","").trim());
-            if(res !== null){
-              this.inline("cd:" + res);
-            }
+            this.cd(val.replace("cd","").trim());
+          }
+          else if(command === "ls"){
+            this.ls(val.replace("ls","").trim());
           }
 
           _prompted_helper.toArray(this.elem.getElementsByClassName("prompted-prompt")).reverse()[0].innerHTML = "<span class=\"prompted-accent-1\">" + this.prompt + "</span>" + "<span class=\"prompted-accent-2\">" + this.path + "</span>";
@@ -97,7 +103,77 @@ function _prompted(elem, options){
   }.bind(this), false);
 }
 
-_prompted.prototype.inline = function(text){
+_prompted.prototype.findFiles = function(path){
+  //might have *
+  //replace all * with (.*?), then make regex
+
+  _path = path.split("/");
+
+  //var r = new RegExp(path.split("*").join("(.*?)"));
+
+
+  //return all data things in which the parents match
+  var ret = [];
+  for(var i = 0; i < this.data.length; i++){
+    var path = this.data[i].path.split("/");
+    var ok = true;
+    if(_path.length !== path.length){
+      ok = false;
+    }
+    else{
+      for(var j = 0; j < _path.length; j++){
+        var r = new RegExp(_path[j].split("*").join("(.*?)"));
+        if(r.test(path[j]) === false){ok = false}
+      }
+    }
+
+    if(ok === true){
+      ret.push(this.data[i]);
+    }
+  }
+  return ret;
+};
+
+_prompted.prototype.ls = function(arg){
+  var files = [];
+  if(arg === ""){
+    //this.path
+    if(this.path === "/"){
+      files = this.findFiles("/*");
+    }
+    else{
+      files = this.findFiles(this.path + "/*");
+    }
+  }
+  else{
+    //something else...resolve to this.path
+    r_path = this.resolve(this.path, arg);
+    if(r_path === "/"){
+      files = this.findFiles("/*");
+    }
+    else{
+      files = this.findFiles(r_path + "/*");
+    }
+  }
+
+  html = "<ul class=\"flex-list\">";
+  for(var i = 0; i < files.length; i++){
+    if(files[i].folder === true){
+      html += "<li class=\"prompted-accent-2\">" + files[i].name + "</li>";
+    }
+    else if(this.specialExt.indexOf(_prompted_helper.ext(files[i].name)) !== -1){
+      html += "<li class=\"prompted-accent-3\">" + files[i].name + "</li>";
+    }
+    else{
+      html += "<li>" + files[i].name + "</li>";
+    }
+  }
+
+  html += "</ul>";
+  this.insert(html);
+};
+
+_prompted.prototype.print = function(text){
   //display inline text!
   var newNode = document.createElement("div");
   newNode.className = "prompted-row";
@@ -106,44 +182,53 @@ _prompted.prototype.inline = function(text){
   this.elem.insertBefore(newNode, last_row);
 };
 
-_prompted.prototype.cd = function(arg){
-  //first, resolve path
+_prompted.prototype.insert = function(html){
+  var newNode = document.createElement("div");
+  newNode.className = "prompted-row";
+  newNode.innerHTML = html;
+  var last_row = _prompted_helper.toArray(this.elem.getElementsByClassName("prompted-row")).reverse()[0];
+  this.elem.insertBefore(newNode, last_row);
+};
 
-  var path = this.path;
-  if(arg[0] === "/"){path="";arg=arg.replace("/","")} //remove leading /
-  if(arg[0] === "~"){path="";arg=arg.replace("~","")}
-  arg = arg.split("/");
-  for(var i = 0; i < arg.length; i++){
-    if(arg[i] !== "." && arg[i] !== ""){
-      //not nothing
-      if(arg[i] === ".."){
-        if(path === ""){
-          //nothing...
-        }
-        else{
-          //remove the last one
-          path = path.split("/").reverse().slice(1).reverse().join("/");
-        }
-      }
-      else{
-        //just something normal
-        if(this.canCd(path + "/" + arg[i])){
-          path = path + "/" + arg[i];
-        }
-        else{
-          return "This folder does not exist";
-        }
-      }
+_prompted.prototype.resolve = function(path, cd){
+
+  //add these on at the end
+  if(cd[0] === "~"){cd=cd.replace("~","/")} //will be taken care of...
+  if(path[0] === "/"){path=path.replace("/","")}
+  if(cd[0] === "/"){cd=cd.replace("/","");path=""}
+
+  path = path.split("/").join(" ").trim().split(" ");
+  cd = cd.split("/").join(" ").trim().split(" ");
+  for(var i = 0; i < cd.length; i++){
+    if(cd[i] === ".."){
+      path = path.reverse().slice(1).reverse();
+    }
+    else if(cd[i] !== "." && cd[i].trim() !== ""){
+      //no change if . or ..
+      path.push(cd[i]);
     }
   }
 
-  //if nothing happens...
-  if(path[0] === "/"){path=path.replace("/","")}
-  this.path = path;
-  return null;
+  return "/" + path.join(" ").trim().split(" ").join("/");
+};
+
+_prompted.prototype.cd = function(arg){
+  //resolve everything along the way
+  cd = arg.split("/");
+  if(cd[0] === ""){cd[0]="/"}
+  if(arg[0] === "~"){cd[0]="~"}
+  for(var i = 1; i < cd.length; i++){
+    var r_path = this.resolve(this.path, cd.slice(0,i).join("/"));
+    if(this.canCd(r_path) === false){
+      this.print("cd: this folder was not found");
+      return;
+    }
+  }
+  this.path = this.resolve(this.path, arg);
 };
 
 _prompted.prototype.canCd = function(path){
+  if(path === "/"){return true}
   for(var i = 0; i < this.data.length; i++){
     if(this.data[i].path === path){
       if(this.data[i].folder === true){
@@ -156,7 +241,6 @@ _prompted.prototype.canCd = function(path){
 }
 
 _prompted.prototype.readTree = function(tree, path){
-  if(path[0] === "/"){path = path.replace("/","")} //don't start with a "/"
   //initial population
   for(var i = 0; i < tree.length; i++){
     /*
@@ -169,16 +253,11 @@ _prompted.prototype.readTree = function(tree, path){
     */
 
     var to_push = {};
-    to_push.path = tree[i].path;
-    if(tree[i].path.indexOf("/") === -1){
-      to_push.name = tree[i].path;
-      to_push.parent = path;
-      to_push.path = path + "/" + to_push.path
-    }
-    else{
-      to_push.name = tree[i].path.split("/").reverse()[0];
-      to_push.parent = path + "/" + tree[i].path.split("/").reverse().slice(1).reverse().join("/");
-    }
+
+    to_push.path = this.resolve(path, tree[i].path);
+    to_push.name = to_push.path.split("/").reverse()[0];
+    to_push.parent = to_push.path.split("/").reverse().slice(1).reverse().join("/");
+    if(to_push.parent === ""){to_push.parent === "/"}
 
     to_push.folder = tree[i].folder;
     if(to_push.folder === false){
@@ -188,8 +267,6 @@ _prompted.prototype.readTree = function(tree, path){
       //a folder with contents
       this.readTree(tree[i].contents, tree[i].path);
     }
-
-    if(to_push.parent[0] === "/"){to_push.parent = to_push.parent.replace("/","")}
 
     this.data.push(to_push);
   }
@@ -231,3 +308,12 @@ _prompted_helper.toArray = function(nl){
   for(var i = nl.length; i--; arr.unshift(nl[i]));
   return arr;
 }
+
+_prompted_helper.ext = function(name){
+  if(name.indexOf(".") === -1){
+    return null;
+  }
+  else{
+    return name.split(".").reverse()[0];
+  }
+};
